dd491c91ed1fff07ee7a582a152dd9589d760b26
RudraPratap-CodeRunner <rp.rudrapratap.official@gmail.com>
2025-09-06 11:34:58 +0530

    Feed.jsx page completed with components StoryModel.jsx, StoriesBar.jsx and StoryViewer.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,11 +10,13 @@ import Profile from './pages/Profile';
 import CreatePost from './pages/CreatePost';
 import { useUser } from '@clerk/clerk-react';
 import Layout from './pages/Layout';
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   const {user} = useUser();
   return (
-    <div>
+    <>
+      <Toaster />
       <Routes>
         <Route path='/' element={!user ? <Login/> : <Layout/> }>
           <Route index element={<Feed/>} />
@@ -28,7 +30,7 @@ const App = () => {
          
         </Route>
       </Routes>
-    </div>
+    </>
   )
 }
 
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading'
+import StoriesBar from '../components/StoriesBar';
 
 const Feed = () => {
-  return (
-    <div>
-      
+  const [feeds,setfeeds] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  const fetchFeeds = async ()=>{
+    setfeeds(dummyPostsData);
+  }
+
+  useEffect(()=>{
+    fetchFeeds();
+    setLoading(false);
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list  */}
+      <div>
+        <StoriesBar/>
+        <div className='py-4 space-y-6'>
+          lis of post
+        </div>
+      </div>
+      {/* right sidebar  */}
+      <div>
+        <div>
+          <h1>sponsered</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
     </div>
-  )
+  ) : <Loading/>
 }
 
 export default Feed
